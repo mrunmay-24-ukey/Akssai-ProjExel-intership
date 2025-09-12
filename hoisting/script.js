10f52edeb3d2adff01b5e6f97d4d1709d7980685
mrunmay-24-ukey <112476865+mrunmay-24-ukey@users.noreply.github.com>
2025-09-12 14:10:46 +0530

    hosting

---
+++ hoisting/script.js
@@ -0,0 +1,26 @@
+try {
+    console.log(username)
+    
+} catch (error) {
+    console.log('username error : ' ,error)
+}
+
+let username = 'mayur'
+
+console.log(city)
+var city = 'delhi'
+
+hello()
+
+function hello(){
+    console.log('hello world')
+}
+
+
+console.log(users)
+
+var users = {
+    name : 'Akash',
+    roll : 100
+}
+
