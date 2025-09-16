d401b3b2b3b86024b5904d1a06ca672ac6b51184
mrunmay-24-ukey <112476865+mrunmay-24-ukey@users.noreply.github.com>
2025-09-16 16:52:02 +0530

    innerText,innerHTML,textContent

---
+++ DOM/Selectors/script.js
@@ -0,0 +1,13 @@
+const heading = document.getElementById('#heading')
+console.log(heading)
+
+// getElementsByTagName()
+// getElementsByClassName()
+// getElementById()
+// getElementByClass()
+
+// getElementById mein agar koi aisi id de denge wo exist nhi karti document mein to return value null hoti hai 
+
+// yeh NodeList return karte hai direct HTML element return nhi karte 
+// querySelectorAll()
+
---
+++ DOM/innerText,TextContent/script.js
@@ -0,0 +1,5 @@
+const para = document.querySelector('#paragraph')
+console.log(para)
+
+// textContent mein spaces wager bhi dikhta hai 
+// innerText nhi dikhata 
